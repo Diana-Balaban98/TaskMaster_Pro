95a59d58f30defd54f897dec316e2287e23de797
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-08-02 22:48:50 +0300

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            Hello
+            App with useState()
         </div>
     );
 };
--- src/AppWithReducer.tsx
+++ src/AppWithReducer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AppWithReducer = () => {
     return (
         <div>
-            Hello
+          AppWithReducer with  useReducer()
         </div>
     );
 };
